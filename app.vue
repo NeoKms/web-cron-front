5c1191b137a93dbe62b2d17f2b0f3352dda73a7a
Zhigulskiy Vladislav <upachko@gmail.com>
2023-04-19 18:00:28 +0300

    no message

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-      <AppAlert />
+    <AppAlert />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,26 @@
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/eslint-module',
-        '@pinia/nuxt',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/i18n',
-        '@nuxtjs/device',
-        '@element-plus/nuxt'
-    ],
-    eslint: {
-        /* module options */
-    }
-    // elementPlus: { /** Options */ },
-    // i18n: {
-    //     vueI18n: '@/i18n.config.ts
-    // },
-    // app: {
-    //     head: {
-    //         charset: 'utf-8',
-    //         viewport: 'width=device-width, initial-scale=1',
-    //     },
-    //     pageTransition: { name: 'page', mode: 'out-in' }
-    // }
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    },
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    '@nuxtjs/device',
+    '@element-plus/nuxt'
+  ],
+  eslint: {},
+  i18n: {
+    vueI18n: './plugins/i18n/i18n.config.ts'
+  },
+  elementPlus: {
+    icon: 'ElIcon',
+    importStyle: 'scss',
+    themes: ['dark']
+  }
 })
