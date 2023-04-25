d5d9d2ded155b70cfa615f4f434594fad4abfb3c
Zhigulskiy Vladislav <upachko@gmail.com>
2023-04-25 11:05:05 +0300

    no message

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,11 +1,27 @@
+import { mdiViewDashboardVariant } from '@mdi/js';
+
 export interface ResultWithMessage<T> {
-    message: string;
-    result: T;
-    error?: string;
+  message: string;
+  result: T;
+  error?: string;
 }
 
 export interface EnvConfig {
-    API_HOST: string;
-    PRODUCTION: boolean;
-    AUTH_COOKIE_NAME: string;
+  API_HOST: string;
+  PRODUCTION: boolean;
+  AUTH_COOKIE_NAME: string;
+}
+
+export interface UserNavigationItem {
+  name: string;
+  href?: string;
+  action?: () => any;
+}
+
+export interface NavigationItem {
+  name: string;
+  href: string;
+  current: boolean;
+  icon: string;
+  skip?: boolean;
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
   <div v-if="!!user" class="min-h-screen">
-    <Disclosure v-slot="{ open }" as="nav" class="bg-gray-800 shadow-lg">
+    <Disclosure v-slot="{ open, close }" as="nav" class="bg-gray-800 shadow-lg">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="-mr-2 flex md:hidden">
             <!-- Mobile menu button -->
-            <DisclosureButton class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <DisclosureButton
+              class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
               <span class="sr-only">Open main menu</span>
               <common-my-svg-icon v-if="!open" :path="mdiMenu" class="block h-6 w-6" type="mdi" />
               <common-my-svg-icon v-else :path="mdiClose" class="block h-6 w-6" type="mdi" />
@@ -21,21 +23,24 @@
           </div>
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
-              <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <button
+                type="button"
+                class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
                 <span class="sr-only">View notifications</span>
                 <common-my-svg-icon class="h-6 w-6" :path="mdiBellOutline" type="mdi" />
               </button>
               <!-- Profile dropdown -->
-              <Menu as="div" class="relative ml-3">
+              <Menu as="div" class="relative ml-3 hover:text-white text-gray-400">
                 <div>
                   <MenuButton class="px-3 flex max-w-xs items-center rounded-full bg-gray-800 text-sm">
                     <span class="sr-only">Open user menu</span>
                     <div class="flex-shrink-0">
-                      <common-my-svg-icon class="text-white" :path="mdiAccount" type="mdi" />
+                      <common-my-svg-icon :path="mdiCardAccountDetailsOutline" type="mdi" />
                     </div>
                     <div class="ml-3">
                       <div class="text-base font-medium leading-none text-white">
-                        {{ user.fio }}
+                        {{ fioSmall }}
                       </div>
                       <div class="text-sm font-medium leading-none text-gray-400">
                         {{ user.email }}
@@ -51,9 +56,14 @@
                   leave-from-class="transform opacity-100 scale-100"
                   leave-to-class="transform opacity-0 scale-95"
                 >
-                  <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                      <nuxt-link :to="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 cursor-pointer']" @click="item.action">
+                  <MenuItems
+                    class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  >
+                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active, close: clMenu }">
+                      <nuxt-link
+                        :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 cursor-pointer']"
+                        @click="handleClickUserNav(item,clMenu)"
+                      >
                         {{ item.name }}
                       </nuxt-link>
                     </MenuItem>
@@ -72,55 +82,70 @@
         leave-from-class="transform opacity-100 scale-100"
         leave-to-class="transform opacity-0 scale-95"
       >
-        <DisclosurePanel class="md:hidden absolute top-[3.9rem] w-full bg-gray-800">
+        <DisclosurePanel class="z-10 md:hidden absolute top-[3.9rem] w-full bg-gray-800">
           <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3 ">
-            <DisclosureButton
+            <nuxt-link
               v-for="item in navigation"
               :key="item.name"
-              as="a"
-              :href="item.href"
-              :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'space-x-4 relative items-center flex  block rounded-md px-3 py-2 text-base font-medium']"
+              :to="item.href"
+              :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'space-x-4 relative items-center flex rounded-md px-3 py-2 text-base font-medium']"
               :aria-current="item.current ? 'page' : undefined"
+              @click="close()"
             >
               <common-my-svg-icon :path="item.icon" type="mdi" />
               <span class="-mr-1 font-medium">{{ item.name }}</span>
-            </DisclosureButton>
+            </nuxt-link>
           </div>
           <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">
               <div class="flex-shrink-0">
-                <common-my-svg-icon class="text-white" :path="mdiAccount" type="mdi" />
+                <common-my-svg-icon class="text-white" :path="mdiCardAccountDetailsOutline" type="mdi" />
               </div>
               <div class="ml-3">
                 <div class="text-base font-medium leading-none text-white">
-                  {{ user.fio }}
+                  {{ fioSmall }}
                 </div>
                 <div class="text-sm font-medium leading-none text-gray-400">
                   {{ user.email }}
                 </div>
               </div>
-              <button type="button" class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <button
+                type="button"
+                class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
                 <span class="sr-only">View notifications</span>
                 <common-my-svg-icon class="h-6 w-6" :path="mdiBellOutline" type="mdi" />
               </button>
             </div>
             <div class="mt-3 space-y-1 px-2">
-              <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
+              <nuxt-link
+                v-for="item in userNavigation"
+                :key="item.name"
+                :to="item.href"
+                class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                @click="close();item.action()"
+              >
                 {{ item.name }}
-              </DisclosureButton>
+              </nuxt-link>
             </div>
           </div>
         </DisclosurePanel>
       </transition>
     </Disclosure>
     <div class="flex">
-      <div class="z-10 hidden md:block absolute sidebar w-[3.35rem] overflow-hidden border-r hover:w-[200px] hover:bg-white hover:shadow-lg">
+      <div
+        class="z-10 hidden md:block absolute sidebar w-[3.35rem] overflow-hidden border-r hover:w-[200px] hover:bg-white hover:shadow-lg"
+      >
         <div class="flex h-full flex-col justify-between pb-6">
           <div>
             <ul class="space-y-2 tracking-wide">
               <template v-for="item in navigation">
                 <li v-if="!item.skip" :key="item.name" class="min-w-max">
-                  <nuxt-link :to="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-500 hover:bg-gray-700 hover:text-white', 'space-x-4 relative items-center flex px-4 py-3 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">
+                  <nuxt-link
+                    :to="item.href"
+                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-500 hover:bg-gray-700 hover:text-white', 'space-x-4 relative items-center flex px-4 py-3 text-sm font-medium']"
+                    :aria-current="item.current ? 'page' : undefined"
+                  >
                     <common-my-svg-icon :path="item.icon" type="mdi" />
                     <span class="-mr-1 font-medium">{{ item.name }}</span>
                   </nuxt-link>
@@ -132,7 +157,11 @@
             <ul class="space-y-2 tracking-wide">
               <template v-for="item in navigation">
                 <li v-if="item.skip" :key="item.name" class="min-w-max">
-                  <nuxt-link :to="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-500 hover:bg-gray-700 hover:text-white', 'space-x-4 relative items-center flex px-4 py-3 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">
+                  <nuxt-link
+                    :to="item.href"
+                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-500 hover:bg-gray-700 hover:text-white', 'space-x-4 relative items-center flex px-4 py-3 text-sm font-medium']"
+                    :aria-current="item.current ? 'page' : undefined"
+                  >
                     <common-my-svg-icon :path="item.icon" type="mdi" />
                     <span class="-mr-1 font-medium">{{ item.name }}</span>
                   </nuxt-link>
@@ -152,7 +181,7 @@
 import {
   mdiAccount,
   mdiBellOutline,
-  mdiCalendarCheck,
+  mdiCalendarCheck, mdiCardAccountDetailsOutline,
   mdiClose,
   mdiCog, mdiFormatListChecks,
   mdiMenu,
@@ -160,20 +189,55 @@ import {
   mdiViewDashboardVariant
 } from '@mdi/js';
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
-import { useRoute } from '#app';
+import { useRoute, useRouter } from '#app';
 import { useAuthStore } from '~/store/auth';
+import { NavigationItem, UserNavigationItem } from '~/interfaces';
+import { ResponseUserDto } from '~/interfaces/apiTypes/user/dto/response-user.dto';
+
 const route = useRoute();
+const router = useRouter();
 const authStore = useAuthStore();
-const user = computed(() => authStore.user);
-const navigation = ref([
-  { name: 'Dashboard', href: '/', current: route.fullPath === '/', icon: mdiViewDashboardVariant },
-  { name: 'Logs', href: '/logs', current: false, icon: mdiFormatListChecks },
-  { name: 'Jobs', href: '/jobs', current: false, icon: mdiCalendarCheck },
-  { name: 'Servers', href: '/ssh', current: false, icon: mdiServerNetwork },
-  { name: 'Users', href: '/users', current: false, icon: mdiAccount },
-  { name: 'Settings', href: '/settings', current: false, icon: mdiCog, skip: true }
+const user = computed<ResponseUserDto | null>(() => authStore.user);
+const navigation = ref<NavigationItem[]>([
+  {
+    name: 'Dashboard',
+    href: '/',
+    current: route.fullPath === '/',
+    icon: mdiViewDashboardVariant
+  },
+  {
+    name: 'Logs',
+    href: '/logs',
+    current: false,
+    icon: mdiFormatListChecks
+  },
+  {
+    name: 'Jobs',
+    href: '/jobs',
+    current: false,
+    icon: mdiCalendarCheck
+  },
+  {
+    name: 'Servers',
+    href: '/ssh',
+    current: false,
+    icon: mdiServerNetwork
+  },
+  {
+    name: 'Users',
+    href: '/users',
+    current: false,
+    icon: mdiAccount
+  },
+  {
+    name: 'Settings',
+    href: '/settings',
+    current: false,
+    icon: mdiCog,
+    skip: true
+  }
 ]);
-const checkCurrentNav = () => {
+const checkCurrentNav = (): void => {
   navigation.value.forEach((e) => {
     e.current = false;
   });
@@ -184,20 +248,44 @@ const checkCurrentNav = () => {
     !!exist && (exist.current = true);
   }
 };
+const fioSmall = computed<string>(() => {
+  if (user?.value?.fio) {
+    const splt = user?.value.fio.split(' ');
+    splt.pop();
+    return splt.join(' ');
+  } else {
+    return '';
+  }
+});
+const handleClickUserNav = (item: UserNavigationItem, close: () => void) => {
+  close();
+  if (item.action) {
+    item.action();
+  } else if (item.href) {
+    router.push(item.href);
+  }
+};
 checkCurrentNav();
 watch(route, checkCurrentNav);
 const currentRoute = computed(() => navigation.value.find(e => e.current) ?? { name: '' });
-const userNavigation = [
-  { name: 'Your Profile', href: '/profile' },
-  { name: 'Sign out', action: authStore.logout }
-];
+const userNavigation = ref<UserNavigationItem[]>([
+  {
+    name: 'Your Profile',
+    href: '/profile'
+  },
+  {
+    name: 'Sign out',
+    action: authStore.logout
+  }
+]);
 </script>
 
 <style scoped lang="scss">
 .sidebar {
-  transition:all .2s ease-in-out;
+  transition: all .2s ease-in-out;
   height: calc(100vh - 4rem);
 }
+
 main {
   height: calc(100vh - 4rem);
 }
