8fc0a0326c627386dfab2667ce8b8e18c7c3817f
Zhigulskiy Vladislav <upachko@gmail.com>
2023-04-26 12:12:21 +0300

    no message

--- pages/login/reset.vue
+++ pages/login/reset.vue
@@ -2,11 +2,11 @@
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-30 w-auto" src="~/assets/img/logo.svg" alt="WebCron">
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      <h2 class="mt-10 text-center text-xl sm:text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Восстановление учётной записи
       </h2>
     </div>
-    <div class="mt-10 mx-auto sm:w-full sm:max-w-md shadow-sm shadow-purple-300 p-10 rounded-3xl">
+    <div class="mt-10 mx-auto w-full sm:w-full sm:max-w-md shadow-sm shadow-purple-300 p-10 rounded-3xl">
       <div v-if="!success" class="space-y-6">
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
@@ -27,7 +27,7 @@
           <button
             :disabled="loading"
             type="submit"
-            class="items-center flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            class="h-8 items-center flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             @click="sendReset"
           >
             <common-loading-circle v-if="loading" />
@@ -36,9 +36,12 @@
         </div>
       </div>
       <div v-else class="space-y-6">
-        <div>
+        <p>
           На указанную почту отправлена ссылка для сброса пароля.
-        </div>
+        </p>
+        <p>
+          Можете закрыть страницу.
+        </p>
       </div>
     </div>
   </div>
